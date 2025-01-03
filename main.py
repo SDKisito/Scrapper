2677e0c063a8e9ef946d3e86ad1d67422dea9c03
Saliou DIEDHIOU <133891956+SDKisito@users.noreply.github.com>
2025-01-03 13:06:46 +0100

    main.py

--- main.py
+++ main.py
@@ -1,19 +1,22 @@
-# main.py
-import requests
-from bs4 import BeautifulSoup
-
-def scrape_website(url):
-    # Envoyer une requête GET pour récupérer le contenu du site
-    response = requests.get(url)
-    
-    if response.status_code == 200:
-        # Parser le contenu HTML du site
-        soup = BeautifulSoup(response.content, 'html.parser')
-        
-        # Extraire tous les paragraphes <p>
-        paragraphs = soup.find_all('p')
-        text_content = ' '.join([para.get_text() for para in paragraphs])
-        
-        return text_content
-    else:
-        return "Échec de la récupération du contenu."
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+# Function to scrape HTML from a website
+def scrape_website(website_url):
+    # Path to WebDriver
+    webdriver_path = "./chromedriver"  # Replace with your WebDriver path
+    service = Service(webdriver_path)
+    driver = webdriver.Chrome(service=service)
+    try:
+        # Open the website
+        driver.get(website_url)
+        # Wait for the page to fully load
+        WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.TAG_NAME, "body")))
+        # Extract the HTML source
+        html_content = driver.page_source
+        return html_content
+    finally:
+        # Ensure the browser is closed after scraping
+        driver.quit()
